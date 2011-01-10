bbd5d2ee481b48b6dad6db31abbf9a3eaa91e838
aclark <aclark@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-01-10 10:44:36 +0000

    Support alternate ssh ports

--- collective/recipe/rsync/__init__.py
+++ collective/recipe/rsync/__init__.py
@@ -14,15 +14,23 @@ class Recipe(object):
         self.buildout, self.name, self.options = buildout, name, options
         source = options['source']
         target = options['target']
+
         cmd = '  rsync -av --partial --progress %s %s' % (source, target)
+
+        if port in options:
+            port = options['port']
+            cmd = '  rsync -e `ssh -p %s` -av --partial --progress %s %s' % (port, source, target)
 
         print line
         print 'Running rsync...'
         print cmd
         print '  this may take a while!'
+
         print getoutput('rsync -av --partial --progress %s %s' % (
             source, target))
+
         print 'Done.'
+
         print line
 
     def install(self):
