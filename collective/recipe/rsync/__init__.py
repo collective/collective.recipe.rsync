6015c8017181920b1b79c2221c4f283e1b5e9410
aclark <aclark@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-01-20 23:03:01 +0000

    Kick off development of "script" option (in which c.r.rsync will optionally create a bin/rsync script to copy data, rather than having to execute bin/buildout to copy data)

--- collective/recipe/rsync/__init__.py
+++ collective/recipe/rsync/__init__.py
@@ -14,13 +14,18 @@ class Recipe(object):
         self.buildout, self.name, self.options = buildout, name, options
         source = options['source']
         target = options['target']
-
-        cmd = '  rsync -av --partial --progress %s %s' % (source, target)
-
         if 'port' in options:
             port = options['port']
+            self.rsync(source, target, port=port)
+        else:
+            self.rsync(source, target, port=None)
+
+    def rsync(self, source, target, port=None)
+        if port:
             cmd = "  rsync -e 'ssh -p %s' -av --partial --progress %s %s" % (
                 port, source, target)
+        else:
+            cmd = '  rsync -av --partial --progress %s %s' % (source, target)
 
         print line
         print 'Running rsync...'
@@ -30,13 +35,21 @@ class Recipe(object):
         print 'Done.'
         print line
 
+
     def install(self):
         """Installer"""
         # XXX Implement recipe functionality here
 
         # Return files that were created by the recipe. The buildout
         # will remove all returned files upon reinstall.
-        return tuple()
+
+        # http://pypi.python.org/pypi/zc.buildout#the-scripts-function
+        create_scripts([('rsync', '__init__', 'rsync')],
+            working_set, executable, bindir, arguments=None)
+
+        bindir = self.buildout['buildout']['bin-directory']
+        return tuple((bindir + '/' + 'rsync',))
+
 
     def update(self):
         """Updater"""
