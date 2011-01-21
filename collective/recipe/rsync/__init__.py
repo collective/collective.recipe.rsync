11f5b0aa2d029293eadc334f34a836ba9e9e0e25
aclark <aclark@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-01-21 00:51:37 +0000

    Make it work

--- collective/recipe/rsync/__init__.py
+++ collective/recipe/rsync/__init__.py
@@ -2,9 +2,28 @@
 """Recipe rsync"""
 
 from commands import getoutput
+from pkg_resources import working_set
+from sys import executable
+from zc.buildout.easy_install import scripts as create_script
+
 
 line = ('----------------------------------------' +
         '----------------------------------------')
+
+def rsync(source=None, target=None, port=None):
+    if port:
+        cmd = "  rsync -e 'ssh -p %s' -av --partial --progress %s %s" % (
+            port, source, target)
+    else:
+        cmd = '  rsync -av --partial --progress %s %s' % (source, target)
+
+    print line
+    print 'Running rsync...'
+    print cmd
+    print '  this may take a while!'
+    print getoutput(cmd)
+    print 'Done.'
+    print line
 
 
 class Recipe(object):
@@ -12,42 +31,29 @@ class Recipe(object):
 
     def __init__(self, buildout, name, options):
         self.buildout, self.name, self.options = buildout, name, options
-        source = options['source']
-        target = options['target']
+        self.source = options['source']
+        self.target = options['target']
         if 'port' in options:
-            port = options['port']
-            self.rsync(source, target, port=port)
+            self.port = options['port']
+            rsync(source=self.source, target=self.target, port=self.port)
         else:
-            self.rsync(source, target, port=None)
+            rsync(source=self.source, target=self.target, port=None)
 
-    def rsync(self, source, target, port=None)
-        if port:
-            cmd = "  rsync -e 'ssh -p %s' -av --partial --progress %s %s" % (
-                port, source, target)
-        else:
-            cmd = '  rsync -av --partial --progress %s %s' % (source, target)
-
-        print line
-        print 'Running rsync...'
-        print cmd
-        print '  this may take a while!'
-        print getoutput(cmd)
-        print 'Done.'
-        print line
 
 
     def install(self):
         """Installer"""
         # XXX Implement recipe functionality here
+        bindir = self.buildout['buildout']['bin-directory']
 
         # Return files that were created by the recipe. The buildout
         # will remove all returned files upon reinstall.
 
-        # http://pypi.python.org/pypi/zc.buildout#the-scripts-function
-        create_scripts([('rsync', '__init__', 'rsync')],
-            working_set, executable, bindir, arguments=None)
 
-        bindir = self.buildout['buildout']['bin-directory']
+        # http://pypi.python.org/pypi/zc.buildout#the-scripts-function
+        create_script([('rsync', 'collective.recipe.rsync.__init__', 'rsync')],
+            working_set, executable, bindir, arguments="source='%s', target='%s', port='%s'" % (self.source, self.target, self.port))
+
         return tuple((bindir + '/' + 'rsync',))
 
 
