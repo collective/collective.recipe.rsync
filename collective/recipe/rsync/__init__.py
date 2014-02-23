d8541f36696f1d511c63a8666931c26af11533b8
Alex Clark <aclark@aclark.net>
2014-02-23 15:56:57 -0500

    Flake 8 ; clean up ; make it work

--- collective/recipe/rsync/__init__.py
+++ collective/recipe/rsync/__init__.py
@@ -2,7 +2,6 @@
 """Recipe rsync"""
 
 import logging
-import sys
 import subprocess
 from pkg_resources import working_set
 from sys import executable
@@ -15,26 +14,42 @@ LOG = logging.getLogger("rsync")
 OPTIONS = '-av --partial --progress'
 
 
-def rsync(source=None, target=None, port=None):
-    if port:
-        cmd = ['rsync', '-e', 'ssh -p %s' % port, '-av', '--partial',
-            '--progress', source, target]
+def rsync(rsync_options=None, source=None, target=None, port=None):
+    """
+    Parse options, build and run command
+    """
+
+    # Parse options
+    if rsync_options is None:
+        rsync_options = OPTIONS.split()
     else:
-        cmd = ['rsync', '-av', '--partial', '--progress', source, target]
+        rsync_options = rsync_options.split()
+    if port:
+        rsync_options += ['-e', 'ssh -p %s' % port]
+
+    # Build cmd
+
+    cmd = rsync_options.insert(0, CMD)
+
     LOG.info(LINE)
     LOG.info('Running rsync with command: ')
     LOG.info('  $ %s' % ' '.join(cmd))
-    LOG.info('  Note: depending on the source file(s) size and location, this may take a while!')
+    LOG.info('  Note: depending on the source file(s) size and location, ')
+    LOG.info('  this may take a while!')
     LOG.info(LINE)
     subprocess.call(cmd)
     LOG.info('Done.')
 
 
 class Recipe(object):
-    """zc.buildout recipe"""
+    """
+    """
 
     def __init__(self, buildout, name, options):
+        """
+        """
         self.buildout, self.name, self.options = buildout, name, options
+        self.rsync_options = options['options']
         self.source = options['source']
         self.target = options['target']
         self.port = None
@@ -46,21 +61,33 @@ class Recipe(object):
                 self.script = True
 
     def install(self):
-        """Installer"""
+        """
+        """
         if self.script:
             bindir = self.buildout['buildout']['bin-directory']
             arguments = "source='%s', target='%s', port=%s"
             create_script(
-                [('%s' % self.name, 'collective.recipe.rsync.__init__', 'rsync')],
-                working_set, executable, bindir, arguments=arguments % (
-                self.source, self.target, self.port))
+                [
+                    (
+                        '%s' % self.name,
+                        'collective.recipe.rsync.__init__', 'rsync')
+                ],
+                working_set,
+                executable,
+                bindir,
+                arguments=arguments % (self.source, self.target, self.port))
             return tuple((bindir + '/' + 'rsync',))
         else:
             # if we make it this far, script option is not set so we execute
             # as buildout runs
-            rsync(source=self.source, target=self.target, port=self.port)
+            rsync(
+                rsync_options=self.rsync_options,
+                source=self.source,
+                target=self.target,
+                port=self.port)
             return tuple()
 
     def update(self):
-        """Updater"""
+        """
+        """
         self.install()
